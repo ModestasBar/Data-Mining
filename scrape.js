f835cef2367f48aa2a3fa905439ce06c19cc73b1
ModestasBar <odekst@gmail.com>
2020-01-23 12:53:06 -0600

    refactor

--- scrape.js
+++ scrape.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
  
-const calenderUrl = 'https://www.norwegian.com/en/booking/flight-tickets/farecalendar/?D_City=OSLALL&A_City=RIX&TripType=1&D_Day=02&D_Month=202005&D_SelectedDay=02&R_Day=03&R_Month=202005&R_SelectedDay=03&dFlight=DY1074OSLRIX&dCabinFareType=1&IncludeTransit=false&CurrencyCode=EUR&message=noFlightsOnDate&processid=39576&mode=ab#/?origin=OSLALL&destination=RIX&outbound=2020-05&adults=1&direct=true&oneWay=true&currency=EUR';
-const initUrl = 'https://www.norwegian.com/en/ipc/availability/avaday?D_City=OSLALL&A_City=RIX&TripType=1&D_Day=03&D_Month=202005&D_SelectedDay=03&R_Day=03&R_Month=202005&R_SelectedDay=03&dFlight=DY1074OSLRIX&dCabinFareType=1&IncludeTransit=false&AgreementCodeFK=-1&CurrencyCode=EUR&rnd=30991&mode=ab';
+const initUrl = 'https://www.norwegian.com/en/'
 
 
 const sortData = (data) => {
@@ -36,23 +35,80 @@ const data = (rawTimeAndData) => {
   });
   const page = await browser.newPage();
 
-  // Calculate calendar days
-  await page.goto(calenderUrl, {waitUntil: 'networkidle0'})
-  const days = await page.evaluate(() => {
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(initUrl, {waitUntil: 'networkidle0'});
+
+  // await page.waitForSelector('#nas-airport-select-dropdown-input-0');
+
+  //Destination
+  await page.evaluate(function() {
+    document.querySelector('input#airport-select-origin').value = ''
+  })
+  await page.type('#airport-select-origin', ' OSL', {delay: 100});
+  await page.click('.airport-select__item--focus');
+  await page.type('#airport-select-destination', ' RIX', {delay: 100});
+  await page.click('.airport-select__item--focus');
+  await page.waitFor(1000);
+
+  //One way
+  await page.waitForSelector('#tripType, .outboundDate');
+  await page.click('#tripType span:nth-child(2) .radio__control-indicator');
+
+  // //Direct only
+  // await page.click('.travel-details nas-dropdown:nth-child(3)');
+  // await page.waitForSelector('.travel-details__actions');
+  // await page.click('.travel-details__dropdown--transit-type .travel-details__actions button:nth-child(2)');
+
+  //Get flight calender
+  await page.click('#resultTypeGroups + div span:nth-child(2) .radio__control-indicator');
+
+  await page.waitFor(500);
+  //Get depart date
+  await page.click('.calendar__input');
+  await page.waitForSelector('tbody button:not([disabled])');
+  await page.click('.pull-right');
+  await page.waitForSelector('.calendar__footer--busy.ng-hide');
+  await page.click('.pull-right');
+  await page.waitForSelector('.calendar__footer--busy.ng-hide');
+  await page.click('.pull-right');
+  await page.waitForSelector('.calendar__footer--busy.ng-hide');
+  await page.click('.pull-right');
+  await page.waitForSelector('.calendar__footer--busy.ng-hide');
+  await page.waitFor(1000)
+  await page.click('.ui-datepicker-content tbody tr:nth-child(1) td:nth-child(5)');
+
+  await page.waitForSelector('.calendar__datepicker.ng-hide');
+  await page.click('#searchButton');
+
+  //Get calender days
+  await page.waitFor(10000);
+
+   const days = await page.evaluate(() => {
       return [...document.querySelectorAll('button:not(.lowfare-calendar__item--empty) .lowfare-calendar__date')]
       .map(val => ('0' + Number(val.innerHTML)).slice(-2));
   })
+  // console.log(days);
+
+  await page.click('tbody tr td:nth-child(6)');
+  // await page.waitForSelector('.nas-continue__action button:not([disabled])');
+  await page.waitFor(20000);
+
+  console.log('not clicked');
+  await page.click('.nas-continue__action button:not([disabled])');
+  console.log('clicked');
+  
+  await page.waitForNavigation();
   // Scrape date for the flight
-  await page.goto(initUrl, {waitUntil: 'networkidle0'})
-  for(let i = 0; i < days.length; i++) {
-    await page.select('#ctl00_MainContent_ipcAvaDay_ipcAvaDaySearchBar_ddlDepartureDay', days[i]);
-    await page.waitForNavigation(); 
+  for(let i = 1; i < days.length; i++) {
     const timeAndAirpor = await page.evaluate(() => {
       return [...document.querySelectorAll('.headerbox td[nowrap], .depdest > div, .arrdest > div, .fareselect.standardlowfare')]
       .map(val => val.innerText);
     })
 
     console.log(data(timeAndAirpor));
+
+    await page.select('#ctl00_MainContent_ipcAvaDay_ipcAvaDaySearchBar_ddlDepartureDay', days[i]);
+    await page.waitForNavigation();
   }
 
   await browser.close();
